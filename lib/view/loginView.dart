f4794b71bcbc7bbebc8d5bfbc9ed2ae03a9ae0f1
Arnav Tripathi <f20200082@pilani.bits-pilani.ac.in>
2022-06-16 12:40:30 +0530

    Step-7

--- lib/view/loginView.dart
+++ lib/view/loginView.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
-
+import 'package:mynotes/utilities/show_error_dialog.dart';
 import 'package:mynotes/constants/routes.dart';
 
 class LoginView extends StatefulWidget {
@@ -99,17 +98,19 @@ class _LoginViewState extends State<LoginView> {
                   Navigator.of(context).pushNamedAndRemoveUntil(notesRoute, (route) => false);
                 } on FirebaseAuthException catch (e) {
                   if (e.code == 'wrong-password') {
-                    devtools.log('Wrong Password');
+                    await showErrorDialog(context, 'Wrong Password');
                   }
                   else if (e.code == 'invalid-email') {
-                    devtools.log("Invalid Email");
+                    await showErrorDialog(context, 'Invalid Email');
+                  }
+                  else if(e.code == 'user-not-found'){
+                    await showErrorDialog(context, 'User Not Found');
                   }
                   else {
-                    devtools.log(e.code);
+                    await showErrorDialog(context, "Error: ${e.code}");
                   }
                 } catch (e) {
-                  devtools.log("Something bad happened");
-                  devtools.log(e.toString());
+                  await showErrorDialog(context, e.toString());
                 }
               },
               child: const Text('Sign In'),
@@ -126,3 +127,4 @@ class _LoginViewState extends State<LoginView> {
     );
   }
 }
+
